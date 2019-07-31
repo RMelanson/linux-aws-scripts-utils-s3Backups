d98c79bbc30ddaae7ee9f2b9c9d1b7223a575453
Robin Melanson <robin.e.melanson@gmail.com>
2019-07-31 00:30:51 -0400

    Update setEnv.sh

--- env/setEnv.sh
+++ env/setEnv.sh
@@ -9,3 +9,12 @@ pkg=S3_BACKUPS
 scriptType="utils"
 parentDir="/tmp/scripts/$scriptType/"
 installDir="$parentDir/$pkg"
+
+pkgOwner=ec2-user
+
+echo Setting External Args
+echo These Arguments Overwrite Default Argument Settings
+for arg in "$@"; do
+  echo setArgs EXECUTING: export $arg
+  export $arg
+done
